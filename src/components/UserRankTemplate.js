5b9804db411e58cf15a252fd8101dbc39864d4d6
Marina Petrova <zayssa@mail.ru>
2020-04-11 21:13:06 +0300

    module3-task1

--- src/components/UserRankTemplate.js
+++ src/components/UserRankTemplate.js
@@ -1,8 +1,9 @@
-export const createUserRankTemplate = () => {
+export const createUserRankTemplate = (rank, userpic) => {
+
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
-      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+      <p class="profile__rating">${rank}</p>
+      <img class="profile__avatar" src="${userpic}" alt="Avatar" width="35" height="35">
     </section>`
   );
 };
